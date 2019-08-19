52d605f93c8c657687a1c44806d6d7915940fa28
hailtondecastro <hailtondecastro@gmail.com>
2019-08-19 20:29:57 -0300

    [skip travis]

---
+++ src/test/java/org/jsonplayback/player/hibernate/nonentities/MasterAWrapper.java
@@ -0,0 +1,16 @@
+package org.jsonplayback.player.hibernate.nonentities;
+
+import org.jsonplayback.player.hibernate.entities.MasterAEnt;
+
+public class MasterAWrapper {
+	private MasterAEnt masterA;
+
+	public MasterAEnt getMasterA() {
+		return masterA;
+	}
+
+	public void setMasterA(MasterAEnt masterA) {
+		this.masterA = masterA;
+	}
+	
+}
